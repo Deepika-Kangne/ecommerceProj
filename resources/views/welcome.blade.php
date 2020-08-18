0c41a35e1b133d218e51309a6b58a2d746457073
Manish Mishra <manish.mishra@zee.esselgroup.com>
2020-08-18 14:29:09 +0530

    manish2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ecommerce Site Manish
+                    ABCDEFGHIJ
                 </div>
 
                 <div class="links">
