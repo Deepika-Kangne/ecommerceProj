860363a861ca8c06a9b93a1fbb2afbe6175329b2
unknown <deepika.k@cintero.com>
2020-08-18 12:43:13 +0530

    Welcome_Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ecommerce Site
                 </div>
 
                 <div class="links">
